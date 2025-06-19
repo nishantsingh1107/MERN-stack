2de40caa7305b395e23f7a04fa2d89e02854980e
nishantsingh1107 <nishantsingh0311@gmail.com>
2025-06-20 00:35:45 +0530

    practice

---
+++ Java-script/practice/DOM/script.js
@@ -0,0 +1,26 @@
+// const idTags = document.getElementById("main-heading");
+// console.dir(idTags);
+
+// const classTags = document.getElementsByClassName("card");
+// console.dir(classTags);
+
+// const Tags = document.getElementsByTagName("button");
+// console.dir(Tags);
+
+
+// const firstP = document.querySelector("p");
+// console.dir(firstP);
+
+// const allP = document.querySelectorAll("p");
+// console.dir(allP);
+
+// const allCardClass = document.querySelectorAll(".card");
+// console.dir(allCardClass);
+
+// const allIdTags = document.querySelector("#main-heading");
+// console.dir(allIdTags);
+
+
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.innerText = mainHeading.innerText+" This is second DOM!!";
+console.log(mainHeading);
